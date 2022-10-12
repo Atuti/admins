2c69e893088e2722364f742b0143f9759721c515
Hobson Mokaya <40859558+Atuti@users.noreply.github.com>
2022-10-12 20:25:51 +0300

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from helios.models import User
-
-class UserAdmin(admin.ModelAdmin):	
-    exclude = ('info', 'token')
-    list_display = ('name', 'user_id')
-
-admin.site.register(User, UserAdmin)
